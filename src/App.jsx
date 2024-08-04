ab16c37d8808ff8e93bf7a61bc9be8afa9ea6409
Rafael Almeida <almeidarp.dev@gmail.com>
2024-08-04 18:13:06 -0300

    feat: adição de componentes, Classes e hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import { StockContextProvider } from "./contexts/StockContext";
 
 export default function App() {
   return (
-    <>
+    <StockContextProvider>
    <RouterProvider router={router} />
-    </>
+   </StockContextProvider>
+   
   )
 }
